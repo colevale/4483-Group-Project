2f9b588ef86ca8a5eee15338b703ba2cc7024f97
Alan K. <N4ich444@gmail.com>
2026-04-19 18:09:55 -0400

    added backgrounds to ending screens
    self explanatory

--- Assets/Scripts/EndingSceneScript.cs
+++ Assets/Scripts/EndingSceneScript.cs
@@ -1,9 +1,13 @@
+using NUnit.Framework;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EndingSceneScript : MonoBehaviour
 {
     public TMP_Text endingText;
+    public List<RawImage> endingBG;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,6 +22,7 @@ public class EndingSceneScript : MonoBehaviour
         {
             endingText.text = "You massacred the Orcs.\nHow do you feel?\nEnding 2/2";
         }
+        endingBG[endingNum].gameObject.SetActive(true);
     }
 
     public void ResetGame()
