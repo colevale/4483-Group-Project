08e9ad0e3dbef2cd97713b89227a22f1c71dfd85
Alan K. <N4ich444@gmail.com>
2026-04-19 17:21:18 -0400

    changed variables back from testing to normal
    changed variables back to normal

--- Assets/Scripts/TerritoryScript.cs
+++ Assets/Scripts/TerritoryScript.cs
@@ -18,10 +18,10 @@ public class TerritoryScript : MonoBehaviour
         if (!(currentProgression > 0 ))
         {
             //initializes it to 1 if it is a new game
-            PlayerPrefs.SetInt("progress", 3);
+            PlayerPrefs.SetInt("progress", 1);
             PlayerPrefs.SetInt("gold", 400);
             //default value matches initialization of 1
-            currentProgression = 3;
+            currentProgression = 1;
         }
 
         //simplified for current prototype
@@ -29,7 +29,7 @@ public class TerritoryScript : MonoBehaviour
         for (int i = 0; i < currentProgression; i++)
         {
             //prevent overrun
-            if(currentProgression < 4)
+            if(i < 4)
             {
                 levels[i].interactable = true;
             }
